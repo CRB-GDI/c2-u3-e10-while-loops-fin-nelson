28ec17d7470e47f0bee4028b643d0f755f3e2512
fin-nelson <Fintan.Nelson@s.capregboces.org>
2024-03-20 11:54:55 -0400

    finished exercise 10

--- Exercise 10/Program.cs
+++ Exercise 10/Program.cs
@@ -21,7 +21,26 @@
             //prompt for and get user choice
             Console.WriteLine("Please enter 1, 2, 3, or 4");
             choice = int.Parse(Console.ReadLine());
-            Console.WriteLine(choice);
+                //Console.WriteLine(choice);
+
+                if (choice == 1)
+                {
+                    Console.WriteLine(" Starting a new game ");
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Loading your game.. jk");
+                }
+                else if (choice == 3)
+                {
+                     Console.WriteLine("Opening you options BUT NOTHING");
+                }
+                else if (choice > 4 || choice < 1)
+                {
+                    Console.WriteLine("NUH UH! I DEMAND  that you enter 1, 2, 3,  or 4!");
+                }
+                Console.WriteLine();
+
             }
         
         }
